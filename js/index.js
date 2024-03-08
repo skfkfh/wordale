2a65b3521147f9cd1e77defd45ba4b9b0eef119b
yongyi <2d2y0103@gmail.com>
2024-03-08 11:20:35 +0900

    [워들 만들기]JavaScript 작업 완료
    간단한 워들 게임을 만들어봤습니다.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,58 @@
+const 정답 = "APPLE";
+
+let timer = 0;
 let index = 0;
 let attempts = 0;
 
 function appStart() {
-  const handleEnterKey = () => {};
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position:absolute; top:40vh; left:38vw; background-color:white; width:200px; height:100px;";
+    document.body.appendChild(div);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts += 1;
+    index = 0;
+  };
+  const gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+
+  const handleEnterKey = () => {
+    let answerNum = 0;
+
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const letter = block.innerText;
+      const answer = 정답[i];
+      if (letter === answer) {
+        answerNum += 1;
+        block.style.background = "#6AAA64";
+      } else if (answer.includes(letter)) block.style.background = "C9B458";
+      else block.style.background = "#787C7E";
+
+      block.style.color = white;
+    }
+    if (answerNum === 5) gameover();
+    else nextLine();
+  };
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      thisBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
 
   const handleKeydown = (event) => {
     const key = event.key.toUpperCase();
@@ -10,15 +60,32 @@ function appStart() {
     const thisBlock = document.querySelector(
       `.board-block[data-index='${attempts}${index}']`
     );
-    if (event.key === "Enter") {
-      handleEnterKey();
-    } else if (index === 5) return;
-    else if (65 <= keyCode && keyCode <= 90) {
+
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
       thisBlock.innerText = key;
       index++;
     }
   };
+  const startTimer = () => {
+    const startTime = new Date();
 
+    function setTime() {
+      const currentTime = new Date();
+      const flowTime = new Date(currentTime - startTime);
+      const minute = flowTime.getMinutes().toString().padStart(2, "0");
+      const second = flowTime.getSeconds().toString().padStart(2, "0");
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${minute}:${second}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
   window.addEventListener("keydown", handleKeydown);
 }
 
